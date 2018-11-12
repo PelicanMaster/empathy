6d75327363fa89547acd70a1e9f722e923c81377
LevavasseurYann <levavasseuryann@gmail.com>
2018-11-12 10:02:10 +0100

    Human: Ethic code building

---
+++ utils/human.py
@@ -0,0 +1,16 @@
+class Human:
+    """
+    Human
+
+    Parameters:
+        * gender: int
+            Gender == 0 || 1
+
+    Variables:
+        * gender: int
+    """
+    def __init__(self, gender):
+        self.gender = gender
+
+    def __repr__(self):
+        return (str(self.gender))
