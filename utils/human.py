9efd1ef776541d66d0f9ec2b262732e523155abc
LevavasseurYann <levavasseuryann@gmail.com>
2018-11-16 09:41:04 +0100

    feature(human.py): add energy variable & sleep func

--- utils/human.py
+++ utils/human.py
@@ -8,12 +8,21 @@ class Human:
 
     Variables:
         * gender: int
+            Biological gender at born
+        * energy: int
+            the capacity for activity, available power.
     """
     def __init__(self, gender):
         self.gender = gender
+        self.energy = 100
 
     def __repr__(self):
         return (str(self.gender))
 
     def eat(self, food):
+        self.energy += 25
         print("Human eat" + str(food))
+
+    def sleep(self, time):
+        self.energy = self.energy + time*10
+        print("Human sleep during " + str(time) + " hours.")
