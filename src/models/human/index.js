2679a8830a0cb35b950c12c5e35fce458551c328
Nathan Patry <nathan.patry@partelya.com>
2018-11-20 19:48:09 +0100

    Added persistence & name to human.

---
+++ src/models/human/index.js
@@ -0,0 +1,25 @@
+export const SEX = {
+    ASEXUAL: 0,
+    MALE: 1,
+    FEMALE: 2,
+    ANDROGYNOUS: 3,
+    UNKNOWN: 99
+};
+
+const DEFAULT_NAME = 'Human';
+
+export default class Human {
+    constructor (sex = SEX.UNKNOWN) {
+        this.sex = sex;
+        this.name = null;
+    }
+
+    eat (food) {
+        return `${this.name || DEFAULT_NAME} eats ${food}`;
+    }
+
+    say (sentence) {
+        return `${this.name || DEFAULT_NAME} growls ${sentence}`;
+    }
+
+}
